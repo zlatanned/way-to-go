d64261c14ce1ea3b3991337a05d9731845ce0002
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-01 14:59:19 +0530

    init hello, world!

---
+++ 1. Getting Started/Hello-world.go
@@ -0,0 +1,10 @@
+// Every Go program is made up of packages.
+// A Go program starts running in the main package.
+// This is why we need to declare our code as the main package -- to make it run and create the output:
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
