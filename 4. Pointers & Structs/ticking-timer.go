c13b7d19517267cefaa1c8d6d668ddc7d33d5f6e
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-06 23:03:25 +0530

    Update ticking-timer.go

--- 4. Pointers & Structs/ticking-timer.go
+++ 4. Pointers & Structs/ticking-timer.go
@@ -1,7 +1,7 @@
 /*
   You are building a Timer app that should count up to a given number.
   Your program needs to take a number as input and make the Timer tick that number of times.
-  
+
   The code in main initializes a Timer and takes a number as input.
   Then it calls the tick() method for the Timer the given number of times.
 
